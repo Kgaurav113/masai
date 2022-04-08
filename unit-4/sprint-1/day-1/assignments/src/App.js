a9da7c8e05e51a62c78295154a77f70cb42f6593
Kumar Gaurav <99576535+Kgaurav113@users.noreply.github.com>
2022-04-08 22:50:07 +0530

    Add files via upload

---
+++ unit-4/sprint-1/day-1/assignments/src/App.js
@@ -0,0 +1,15 @@
+import Button from "./day2/Button"
+import input from "./day2/input"
+
+function App (){
+  return 
+  (
+    <div>
+      <input/>
+      <Button />
+      <List/>
+    </div>
+  )
+
+}
+export default App
---
+++ unit-4/sprint-1/day-1/assignments/src/day2/Button.jsx
@@ -0,0 +1,6 @@
+function Button (){
+    return (
+        <button>Click</button>
+    )
+}
+export default Button
